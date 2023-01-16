1fcc3015419987d6c3ebec8fc75978e0ed01eedd
bilkis01 <bilkiscse@gmail.com>
2023-01-16 15:29:43 +0600

    route setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './component/Item/Home/Home';
+import Navbar from './component/Item/Navbar/Navbar';
+import About from './component/About/About'
+import Menu from './component/Menu/Menu'
+import Blog from './component/Blog/Blog'
+import Pages from './component/Pages/Pages'
+import Contact from './component/Contact/Contact'
 
 function App() {
   return (
-    <div className="App">
-  
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/menu' element={<Menu></Menu>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/pages' element={<Pages></Pages>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+      </Routes>
+
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,21 @@ module.exports = {
   theme: {
     extend: {},
   },
+
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#76459D",
+          secondary: "#f6d860",
+          accent: "#2A435D",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
   plugins: [require("daisyui")],
 }
